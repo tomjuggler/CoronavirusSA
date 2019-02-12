bcd3081ce18d26fff8a252893d7c48209bac2286
Tom Hastings <tomjuggler@gmail.com>
2019-02-12 22:01:08 +0200

    check to see if internet connected, otherwise show offline web page in webview using private post (making public later I think but invisible)
    some useful changes here!
    
    Signed-off-by: Tom Hastings <tomjuggler@gmail.com>

--- app/src/main/java/za/co/tombigtop/playingwithmenuandactivities/WebActivity.java
+++ app/src/main/java/za/co/tombigtop/playingwithmenuandactivities/WebActivity.java
@@ -1,7 +1,10 @@
 package za.co.tombigtop.playingwithmenuandactivities;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -16,22 +19,38 @@ public class WebActivity extends Activity {
         setContentView(R.layout.content_web);
 
         mWebView = (WebView) findViewById(R.id.activity_main_webview);
-
+        mWebView.clearCache(true);
+        mWebView.clearHistory();
         // Force links and redirects to open in the WebView instead of in a browser
         mWebView.setWebViewClient(new WebViewClient());
-
+//        mWebView.setWebChromeClient(new WebChromeClient());
         // Enable Javascript
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-
+//        webSettings.setBuiltInZoomControls(true);
+        //todo: how to check if internet connected and use backup if necessary? -> see below:
+        //doesn't seem to work if wifi connected but no internet...
+        //otherwise good check.
+        //solution from https://stackoverflow.com/questions/17959561/android-how-to-prevent-webview-to-load-when-no-internet-connection
+        //may be a better solution there.
+        if (!checkInternetConnection(this)) {
+            // LOCAL RESOURCE
+            mWebView.loadUrl("file:///android_asset/site.htm");
+        } else{
+//            mWebView.loadUrl("https://circusscientist.com");
+            mWebView.loadUrl("https://www.circusscientist.com/2019/02/10/test-private-post");
+            mWebView.setWebViewClient(new MyWebViewClient());
+        }
         // REMOTE RESOURCE
-        mWebView.loadUrl("https://circusscientist.com");
-        mWebView.setWebViewClient(new MyWebViewClient());
+//        mWebView.loadUrl("https://circusscientist.com");
+//        mWebView.setWebViewClient(new MyWebViewClient());
 
         //need to have this as backup if no internet connection:
-        //todo: how to check if internet connected and use backup if necessary?
+
         // LOCAL RESOURCE
-        // mWebView.loadUrl("file:///android_asset/index.html");
+//         mWebView.loadUrl("file:///android_asset/site.htm");
+//         setContentView(mWebView);
+//        mWebView.setWebViewClient(new MyWebViewClient()); //not necessary?
     }
 
     // Prevent the back-button from closing the app
@@ -44,4 +63,13 @@ public class WebActivity extends Activity {
         }
     }
 
+    public static boolean checkInternetConnection(Context context) {
+
+        ConnectivityManager con_manager = (ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return (con_manager.getActiveNetworkInfo() != null
+                && con_manager.getActiveNetworkInfo().isAvailable()
+                && con_manager.getActiveNetworkInfo().isConnected());
+    }
 }
