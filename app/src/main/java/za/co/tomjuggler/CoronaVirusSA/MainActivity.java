6e61b6e5f1281ed4aa3e9380918b3fa9d715bd88
tom <tomjuggler@gmail.com>
2020-04-14 22:06:02 +0200

    Added Settings webview activity, to download updates Added downloadmanager to webview components, so updates can be downloaded
    Signed-off-by: tom <tomjuggler@gmail.com>

--- app/src/main/java/za/co/tomjuggler/CoronaVirusSA/MainActivity.java
+++ app/src/main/java/za/co/tomjuggler/CoronaVirusSA/MainActivity.java
@@ -57,6 +57,8 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(getApplicationContext(), SettingsWebActivity.class);
+            startActivity(intent);
             return true;
         }
 
--- app/src/main/java/za/co/tomjuggler/CoronaVirusSA/SettingsWebActivity.java
+++ app/src/main/java/za/co/tomjuggler/CoronaVirusSA/SettingsWebActivity.java
@@ -11,7 +11,7 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-public class WebActivity extends Activity {
+public class SettingsWebActivity extends Activity {
 
     private WebView mWebView;
 
@@ -25,27 +25,10 @@ public class WebActivity extends Activity {
         mWebView.clearHistory();
         // Force links and redirects to open in the WebView instead of in a browser
         mWebView.setWebViewClient(new WebViewClient());
-//        mWebView.setWebChromeClient(new WebChromeClient());
-        // Enable Javascript (may need this?)
         WebSettings webSettings = mWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-//        webSettings.setBuiltInZoomControls(true);
-        //todo: how to check if internet connected and use backup if necessary? -> see below:
-        //doesn't seem to work if wifi connected but no internet...
-        //otherwise good check.
-        //solution from https://stackoverflow.com/questions/17959561/android-how-to-prevent-webview-to-load-when-no-internet-connection
-        //may be a better solution there.
-        if (!checkInternetConnection(this)) {
-            // OFFLINE, USE LOCAL RESOURCE:
-            mWebView.loadUrl("file:///android_asset/site.htm");
-        } else{
-            //todo: test digitalocean hosted video play in webview.
-            // REMOTE RESOURCE:
-           //center for disease control website here:
-            mWebView.loadUrl("https://www.circusscientist.com/coronalinksSA/");
-            mWebView.setWebViewClient(new MyWebViewClient());
-        }
-        //hmmm download?////////////////////////////////////////////////
+        webSettings.setJavaScriptEnabled(false);//        webSettings.setBuiltInZoomControls(true);
+         mWebView.loadUrl("file:///android_asset/settings.htm");
+         //hmmm download?////////////////////////////////////////////////
         mWebView.setDownloadListener(new DownloadListener() {
             public void onDownloadStart(String url, String userAgent,
                                         String contentDisposition, String mimetype,
@@ -56,8 +39,6 @@ public class WebActivity extends Activity {
             }
         });
         //////////////////////////////////////////////////////////////////
-
-
     }
 
     // Prevent the back-button from closing the app
@@ -70,6 +51,7 @@ public class WebActivity extends Activity {
         }
     }
 
+    //may need this?
     public static boolean checkInternetConnection(Context context) {
 
         ConnectivityManager con_manager = (ConnectivityManager)
